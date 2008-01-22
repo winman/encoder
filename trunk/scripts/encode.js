0a2399209abbbbb97420204bc08639a1a0e641cc
Mario.Heiderich <Mario.Heiderich@bd3d36c4-f436-0410-a735-c75673d4e21e>
2008-01-22 23:08:12 +0000

    committed the last h4k.in encoder release

--- trunk/scripts/encode.js
+++ trunk/scripts/encode.js
@@ -11,8 +11,6 @@ Encoder.toCharCode = function (field){
 			}
 		}
 		document.getElementById(field + '-text').value = charcode;										
-		document.getElementById(field + '-from-charcode').disabled=false;
-		document.getElementById(field + '-to-charcode').disabled=true;
 	}
 	return false;
 }
@@ -25,8 +23,6 @@ Encoder.fromCharCode = function (field){
 			output += String.fromCharCode(charcode[i]);
 		}					
 		document.getElementById(field + '-text').value = output;	
-		document.getElementById(field + '-from-charcode').disabled=true;
-		document.getElementById(field + '-to-charcode').disabled=false;
 	}
 	return false;
 }
@@ -35,8 +31,6 @@ Encoder.toUrlEncode = function (field){
 	if(text.length > 0){
 		output = encodeURIComponent(text);
 		document.getElementById(field + '-text').value = output;
-		document.getElementById(field + '-from-urlencode').disabled=false;
-		document.getElementById(field + '-to-urlencode').disabled=true;
 	}
 	return false;
 }
@@ -45,8 +39,6 @@ Encoder.fromUrlEncode = function (field){
 	if(text.length > 0){
 		output = decodeURIComponent(text);
 		document.getElementById(field + '-text').value = output;
-		document.getElementById(field + '-from-urlencode').disabled=true;
-		document.getElementById(field + '-to-urlencode').disabled=false;
 	}
 	return false;
 }
@@ -64,6 +56,15 @@ Encoder.toDecEnt = function (field){
     var output = ''		            
     for (var i=0; i<text.length; i++) {
         output += '&#' + text.charCodeAt(i).toString(10) + ';';
+    }
+    document.getElementById(field + '-text').value = output;
+    return false;
+}
+Encoder.toOctEnt = function (field){
+    var text = document.getElementById(field + '-text').value;
+    var output = ''		            
+    for (var i=0; i<text.length; i++) {
+        output += '\\' + text.charCodeAt(i).toString(8);
     }
     document.getElementById(field + '-text').value = output;
     return false;
@@ -98,9 +99,99 @@ Encoder.fromDecEnt = function (field){
     }
     return false;
 }
+Encoder.fromOctEnt = function (field){
+    var text = document.getElementById(field + '-text').value;
+    var array = text.split('\\');
+    var output = '';    
+    for (var i=0; i<array.length; i++) {
+        var node = array[i].replace('\\','');        
+        if(!isNaN(node) && node != ''){
+            output += String.fromCharCode(parseInt(node,8));        
+        }
+    }
+    if(output != ''){
+        document.getElementById(field + '-text').value = output;
+    }
+    return false;
+}
 Encoder.fromVectorSource = function (option, field){
     var exploit = option.options[option.selectedIndex].value;
     document.getElementById(field + '-text').value = exploit;
 
+    return false;
+}
+
+Encoder.fromSQLHex = function(field) {
+
+    var text = document.getElementById(field + '-text').value;
+    var array = text.slice(2).split(/(\w{2})/);
+    var output = '';    
+    for (var i=0; i<array.length; i++) {
+        if(array[i] != '') {
+            var node = parseInt('0x'+array[i]);        
+            if(!isNaN(node) && node != ''){
+                output += String.fromCharCode(node);        
+            }
+        }    
+    }
+    if(output != ''){
+        document.getElementById(field + '-text').value = output;
+    }
+    return false;  
+}
+
+Encoder.toSQLHex = function(field) {
+
+    var hexchars ="0123456789ABCDEF";
+	var text = document.getElementById(field + '-text').value;
+	if(text.length > 0){
+		var charcode = new Array;
+		for(i=0;text.length>i;i++){
+            charcode += hexchars.charAt((text.charCodeAt(i)>>4)&0xf)+hexchars.charAt(text.charCodeAt(i)&0xf)
+		}
+		document.getElementById(field + '-text').value = '0x' + charcode;										
+		document.getElementById(field + '-from-charcode').disabled=false;
+		document.getElementById(field + '-to-charcode').disabled=true;
+	}
+	return false;
+}
+
+Encoder.toSQLChar = function (field){
+    var text = document.getElementById(field + '-text').value;
+    var output = ''		            
+    for (var i=0; i<text.length; i++) {
+        output += 'Char(' + text.charCodeAt(i).toString(10) + '),';
+    }
+    document.getElementById(field + '-text').value = output.substr(0,output.length-1);
+    return false;
+}
+
+Encoder.fromSQLChar = function (field){
+    var text = document.getElementById(field + '-text').value + ',';
+    var array = text.split('),');
+    var output = '';    
+    for (var i=0; i<array.length; i++) {
+        var node = array[i].replace('Char(','');        
+        if(!isNaN(node) && node != ''){
+            output += String.fromCharCode(node);        
+        }
+    }
+    if(output != ''){
+        document.getElementById(field + '-text').value = output;
+    }
+    return false;
+}
+
+Encoder.fromBsToEnt = function(field) {
+    var text = document.getElementById(field + '-text').value;
+    var output = text.replace(/\\(\w{2,5})/ig, '&#$1;'); 
+    document.getElementById(field + '-text').value = output;
+    return false;
+}
+
+Encoder.fromEntToBs = function(field) {
+    var text = document.getElementById(field + '-text').value;
+    var output = text.replace(/&#(\w{2,5});/ig, '\\\$1'); 
+    document.getElementById(field + '-text').value = output;
     return false;
 }
